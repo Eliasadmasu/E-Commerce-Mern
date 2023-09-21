c909ad9abf28e0ed4f7f72d759856fc4b7b94b3d
Elias Admasu <Eliasadmasu1912@gmail.com>
2023-09-21 06:22:30 +0300

    my first commit

---
+++ api/src/index.js
@@ -0,0 +1,10 @@
+import express from "express";
+import "dotenv/config";
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server Started On Port Num: ${PORT}`);
+});
