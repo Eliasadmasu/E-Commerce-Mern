9a2a679744a50b52bc2abb1a46cddd9939513fbf
Elias_adm <55312976+Eliasadmasu@users.noreply.github.com>
2023-10-24 05:27:57 +0300

    Update config.js

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,4 +1,5 @@
 
-const API_URL = "http://localhost:5000";
+// const API_URL = "http://localhost:5000";
+const API_URL = "https://e-commerce-mern-z98o-mdjno04pg-eliasadmasu.vercel.app";
 
 export default API_URL;
