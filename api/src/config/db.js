553cd935fbbeece4706d3755b43535079d47ecee
Elias_adm <55312976+Eliasadmasu@users.noreply.github.com>
2023-10-24 06:19:24 +0300

    Update db.js

--- api/src/config/db.js
+++ api/src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 async function connectToDatabase() {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect(`${process.env.MONGO_URI}`, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
